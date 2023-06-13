5d5d08aa3f27004dd5d5a32c80406aac755eeabd
zaojiashi <117269184+zaojiashi@users.noreply.github.com>
2023-06-13 19:38:58 +0800

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,241 @@
+(function($) {
+    'use strict';
+	// sticky
+	var wind = $(window);
+	var sticky = $('#sticky-header');
+	wind.on('scroll', function () {
+		var scroll = wind.scrollTop();
+		if (scroll < 100) {
+			sticky.removeClass('sticky');
+		} else {
+			sticky.addClass('sticky');
+		}
+	});
+	// Loder  //
+    $(function () {
+        $('body').addClass('loaded');
+    });
+	// Mobile Menu
+    $('.mobile-menu nav').meanmenu({
+        meanScreenWidth: "991",
+        meanMenuContainer: ".mobile-menu",
+        onePage: false,
+    });	
+	<!--barfiller script -->
+
+        $(document).ready(function(){
+        $('#bar1').barfiller({ duration: 7000 });
+        $('#bar2').barfiller({ duration: 7000 });
+        $('#bar3').barfiller({ duration: 7000 });
+        });
+	//Header Search
+	if($('.search-box-outer').length) {
+		$('.search-box-outer').on('click', function() {
+			$('body').addClass('search-active');
+		});
+		$('.close-search').on('click', function() {
+			$('body').removeClass('search-active');
+		});
+	}
+	
+	/*---------------------
+    WOW active js 
+    --------------------- */
+    new WOW().init();
+	
+	/*---------------------
+    Nivo Slider active js 
+    --------------------- */
+    $('#mainSlider').nivoSlider({
+        directionNav: true,
+        animSpeed: 500,
+        slices: 18,
+        pauseTime: 900000,
+        pauseOnHover: false,
+        controlNav: true,
+        prevText: '<i class="fa fa-angle-left nivo-prev-icon"></i>',
+        nextText: '<i class="fa fa-angle-right nivo-next-icon"></i>'
+    });
+    // counterUp
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+     /* Portfolio Isotope  */
+    $('.image_load').imagesLoaded(function() {
+
+        if ($.fn.isotope) {
+
+            var $portfolio = $('.image_load');
+
+            $portfolio.isotope({
+
+                itemSelector: '.grid-item',
+
+                filter: '*',
+
+                resizesContainer: true,
+
+                layoutMode: 'masonry',
+
+                transitionDuration: '0.8s'
+
+            });
+            $('.menu-filtering li').on('click', function() {
+
+                $('.menu-filtering li').removeClass('current_menu_item');
+
+                $(this).addClass('current_menu_item');
+
+                var selector = $(this).attr('data-filter');
+
+                $portfolio.isotope({
+
+                    filter: selector,
+
+                });
+
+            });
+
+        };
+
+    });
+	
+		    // Venubox
+
+    $('.venobox').venobox({
+
+        numeratio: true,
+
+        infinigall: true
+
+    });
+	
+	 // Case Study Active
+    $('.team_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav:false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            },
+            1920: {
+                items: 4
+            }
+        }
+    })	
+	
+ // Testimonial Active
+    $('.testimonial_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: true,
+		margin:0,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1920: {
+                items: 1
+            }
+        }
+    });	
+	// Protfolio Active
+    $('.protfolio_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: false,
+		margin:0,
+        navText: ["<i class='bi bi-arrow-left''></i>", "<i class='bi bi-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            },
+            1920: {
+                items: 4
+            }
+        }
+    });	
+
+ // Brand Active
+    $('.brand_list').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: true,
+        nav: false,
+        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+        responsive: {
+            0: {
+                items: 0
+            },
+			 320: {
+                items: 1
+            },
+			 480: {
+                items: 2
+            },
+			 600: {
+                items: 3
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 5
+            },
+            1000: {
+                items: 6
+            },
+            1920: {
+                items: 6
+            }
+        }
+    })
+
+	/*--------------------------
+     scrollUp
+    ---------------------------- */
+    $.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    })
+})(jQuery);
